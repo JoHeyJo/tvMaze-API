a91141bcffbafe98fe63f31eaab2d38e8a81f678
jf <jpf0628@gmail.com>
2022-03-16 14:34:37 -0700

    WIP: getShowsByTerms

---
+++ tvmaze.js
@@ -0,0 +1,83 @@
+"use strict";
+
+const $showsList = $("#showsList");
+const $episodesArea = $("#episodesArea");
+const $searchForm = $("#searchForm");
+
+
+/** Given a search term, search for tv shows that match that query.
+ *
+ *  Returns (promise) array of show objects: [show, show, ...].
+ *    Each show object should contain exactly: {id, name, summary, image}
+ *    (if no image URL given by API, put in a default image URL)
+ */
+
+async function getShowsByTerm(show) {
+  // ADD: Remove placeholder & make request to TVMaze search shows API.
+  let response = await axios.get(
+    "http://api.tvmaze.com/search/shows", { params: { q:show}}
+  );
+
+  return response.data.map((showData) => {
+    const showImg = showData.image || "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fcliparts.co%2Fcliparts%2FqTB%2FX8p%2FqTBX8p46c.jpg&f=1&nofb=1";
+  return  [showData.show.id, showData.show.name, showData.show.summary, showImg];
+  });
+
+}
+
+
+/** Given list of shows, create markup for each and to DOM */
+
+function populateShows(shows) {
+  $showsList.empty();
+
+  for (let show of shows) {
+    const $show = $(
+        `<div data-show-id="${show.id}" class="Show col-md-12 col-lg-6 mb-4">
+         <div class="media">
+           <img 
+              src="http://static.tvmaze.com/uploads/images/medium_portrait/160/401704.jpg" 
+              alt="Bletchly Circle San Francisco" 
+              class="w-25 me-3">
+           <div class="media-body">
+             <h5 class="text-primary">${show.name}</h5>
+             <div><small>${show.summary}</small></div>
+             <button class="btn btn-outline-light btn-sm Show-getEpisodes">
+               Episodes
+             </button>
+           </div>
+         </div>  
+       </div>
+      `);
+
+    $showsList.append($show);  }
+}
+
+
+/** Handle search form submission: get shows from API and display.
+ *    Hide episodes area (that only gets shown if they ask for episodes)
+ */
+
+async function searchForShowAndDisplay() {
+  const term = $("#searchForm-term").val();
+  const shows = await getShowsByTerm(term);
+
+  $episodesArea.hide();
+  populateShows(shows);
+}
+
+$searchForm.on("submit", async function (evt) {
+  evt.preventDefault();
+  await searchForShowAndDisplay();
+});
+
+
+/** Given a show ID, get from API and return (promise) array of episodes:
+ *      { id, name, season, number }
+ */
+
+// async function getEpisodesOfShow(id) { }
+
+/** Write a clear docstring for this function... */
+
+// function populateEpisodes(episodes) { }
