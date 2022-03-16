42fd0ce11bd709e0adacae8752bdbaa55bf9fb88
jf <jpf0628@gmail.com>
2022-03-16 16:28:59 -0700

    WIP: populateEpisodes eventListener

--- tvmaze.js
+++ tvmaze.js
@@ -3,6 +3,7 @@
 const $showsList = $("#showsList");
 const $episodesArea = $("#episodesArea");
 const $searchForm = $("#searchForm");
+const $episodesList = $("#episodesList");
 
 const DEFAULT_IMG = "https://tinyurl.com/tv-missing";
 const API_BASE_URL = "http://api.tvmaze.com";
@@ -94,8 +95,42 @@ $searchForm.on("submit", async function (evt) {
  *      { id, name, season, number }
  */
 
-async function getEpisodesOfShow(id) {}
+async function getEpisodesOfShow(id) {
+  let tvEpisodes = await axios.get(`${API_BASE_URL}/shows/${id}/episodes`);
+
+  
+  return tvEpisodes.data.map((showData) => {
+    return  { 
+      id: showData.id, 
+      name: showData.name,
+      season: showData.season,
+      number: showData.number
+    };
+  });
+}
 
 /** Write a clear docstring for this function... */
 
-// function populateEpisodes(episodes) { }
+function populateEpisodes(episodes) { 
+  $episodesList.empty();
+
+  for(let episode of episodes){
+    const $episode = $(`
+      <li> ${episode.name} (season: ${episode.season}, number: ${episode.number})"</li>
+    `);
+    $episodesList.append($episode);
+  }
+
+}
+
+
+
+
+
+
+
+//shows
+//http://api.tvmaze.com/search/shows?q=[searchquery]
+
+//episodes
+//http://api.tvmaze.com/shows/[showid]/episodes
